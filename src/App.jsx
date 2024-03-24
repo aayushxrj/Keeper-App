ab48777915b9a18574d47313b4be857fe8c337a9
aayushxrj <aayush.ranjan0904@gmail.com>
2024-03-24 15:06:30 +0530

    now displays dynamic notes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Note from "./components/Note.jsx";
+import {notes} from "./components/notes.js";
 
 
 function App() {
+  function createNotes(noteItem){
+    return (
+        <Note 
+        key = {noteItem.key}
+        title = {noteItem.title}
+        content = {noteItem.content}
+        />
+    )
+}
+
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((noteItem)=> 
+      <Note 
+      key = {noteItem.key} 
+      title = {noteItem.title} 
+      content = {noteItem.content}/>
+      )}
       <Footer />
     </div>
   );
